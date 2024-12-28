b9f3472e8a4e58f0f3a043cfab4144173e651f62
fredyml <113043655+fredyml@users.noreply.github.com>
2024-12-28 15:53:35 -0500

    se crean test

--- reservation-system/src/app/reservations/reservations/reservations.component.spec.ts
+++ reservation-system/src/app/reservations/reservations/reservations.component.spec.ts
@@ -1,23 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReservationsComponent } from './reservations.component';
+import { ReservationsService } from '../../services/reservations.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 describe('ReservationsComponent', () => {
   let component: ReservationsComponent;
   let fixture: ComponentFixture<ReservationsComponent>;
+  let reservationsService: jasmine.SpyObj<ReservationsService>;
 
   beforeEach(async () => {
+    const spyReservationsService = jasmine.createSpyObj('ReservationsService', [
+      'getReservations',
+      'getAvailableSpaces',
+      'getAvailableUsers',
+      'createReservation',
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [ReservationsComponent] 
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, ReactiveFormsModule, FormsModule, ReservationsComponent],
+      providers: [{ provide: ReservationsService, useValue: spyReservationsService }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ReservationsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  }); 
+    reservationsService = TestBed.inject(ReservationsService) as jasmine.SpyObj<ReservationsService>;
 
-  it('should create', () => {
+    reservationsService.getReservations.and.returnValue(of([]));
+    reservationsService.getAvailableSpaces.and.returnValue(of([]));
+    reservationsService.getAvailableUsers.and.returnValue(of([]));
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+ 
+
+  it('should open the modal to create a reservation', () => {
+    component.openModal();
+    expect(component.showModal).toBeTrue();
+    expect(component.isEditing).toBeFalse();
+  });
+
+  it('should close the modal', () => {
+    component.closeModal();
+    expect(component.showModal).toBeFalse();
+    expect(component.isEditing).toBeFalse();
+  });
+
+  it('should apply filters correctly', () => {
+    component.reservations = [
+      { reservationId: 1, spaceName: 'Sala A', userName: 'Juan', startDate: '2023-12-01', endDate: '2023-12-01' },
+      { reservationId: 2, spaceName: 'Sala B', userName: 'Ana', startDate: '2023-12-02', endDate: '2023-12-02' },
+    ];
+    component.filters.spaceName = 'Sala A';
+    component.applyFilters();
+    expect(component.filteredReservations).toEqual([
+      { reservationId: 1, spaceName: 'Sala A', userName: 'Juan', startDate: '2023-12-01', endDate: '2023-12-01' },
+    ]);
   });
 });
--- reservation-system/src/app/services/reservations.service.spec.ts
+++ reservation-system/src/app/services/reservations.service.spec.ts
@@ -1,16 +1,58 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ReservationsService } from './reservations.service';
 
 describe('ReservationsService', () => {
   let service: ReservationsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ReservationsService],
+    });
     service = TestBed.inject(ReservationsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch reservations', () => {
+    const dummyReservations = [{ reservationId: 1, spaceName: 'Sala A', userName: 'Juan' }];
+
+    service.getReservations().subscribe((reservations) => {
+      expect(reservations).toEqual(dummyReservations);
+    });
+
+    const req = httpMock.expectOne(service['apiUrl']);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyReservations);
+  });
+
+  it('should create a reservation', () => {
+    const newReservation = { spaceId: 1, userId: 1, startDate: '2023-12-01T10:00', endDate: '2023-12-01T12:00' };
+
+    service.createReservation(newReservation).subscribe((response) => {
+      expect(response).toEqual(newReservation);
+    });
+
+    const req = httpMock.expectOne(service['apiUrl']);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newReservation);
+    req.flush(newReservation);
+  });
+
+  it('should delete a reservation', () => {
+    const reservationId = 1;
+
+    service.deleteReservation(reservationId).subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}/${reservationId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
   });
 });
